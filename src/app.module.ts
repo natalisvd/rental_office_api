925088b6b7ac1535f5c5786b96e55383d6ee08d1
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-11-12 17:57:33 +0200

    added connection natalisvd/rental_office#1 natalisvd/rental_office#2

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,16 @@ import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 import { OfficeModule } from './office/office.module';
 import { WorkplaceModule } from './workplace/workplace.module';
+import { PurchaseModule } from './purchase/purchase.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AuthModule, OfficeModule, WorkplaceModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    AuthModule,
+    OfficeModule,
+    WorkplaceModule,
+    PurchaseModule,
+  ],
 })
 export class AppModule {}
--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PurchaseController } from './purchase.controller';
 import { PurchaseService } from './purchase.service';
 import { purchaseProviders } from './purchase.provider';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PurchaseController],
   providers: [PurchaseService, ...purchaseProviders],
 })
