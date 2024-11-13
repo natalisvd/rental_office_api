ff74521270b3bfc01ed16922bfb54ccb248b4c69
EvgeniyHalimon <halimonevgeniy@gmail.com>
2024-11-13 16:21:48 +0200

    made cors config natalisvd/rental_office#5

--- src/config.ts
+++ src/config.ts
@@ -1,23 +1,17 @@
 // library
 import * as dotenv from 'dotenv';
-dotenv.config();
+
+dotenv.config({
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+});
 
 export const config = {
-  HOST:
-    process.env.NODE_ENV === 'production'
-      ? process.env.HOST_PROD
-      : process.env.HOST_DEV,
-  DB_PORT:
-    process.env.NODE_ENV === 'production'
-      ? Number(process.env.DB_PORT_PROD)
-      : Number(process.env.DB_PORT_DEV),
-  SERVER_PORT: process.env.SERVER_PORT ?? 3001,
+  HOST: process.env.HOST,
+  DB_PORT: process.env.DB_PORT,
+  SERVER_PORT: process.env.SERVER_PORT,
   SECRET_ACCESS: process.env.SECRET_ACCESS,
   SECRET_REFRESH: process.env.SECRET_REFRESH,
-  EXPIRES_IN: process.env.EXPIRES_IN_ACCESS ?? '',
-  EXPIRES_IN_REFRESH: process.env.EXPIRES_IN_REFRESH ?? '',
-  FE_URL:
-    process.env.NODE_ENV === 'production'
-      ? String(process.env.FE_URL)
-      : 'http://localhost:4200/',
+  EXPIRES_IN: process.env.EXPIRES_IN_ACCESS,
+  EXPIRES_IN_REFRESH: process.env.EXPIRES_IN_REFRESH,
+  FE_URL: String(process.env.FE_URL),
 };
--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,12 @@ import { CustomValidationPipe } from './shared/CustomValidationPipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.setGlobalPrefix('v1/');
-  app.enableCors();
+  app.enableCors({
+    origin: config.FE_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new CustomValidationPipe({
       whitelist: true,
